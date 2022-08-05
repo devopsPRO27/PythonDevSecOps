82a7d14dbfae2b8b69b2fb26c16790b34cf385dd
“hodi <hothaifazoubi@gmail.com>
2022-08-05 12:57:58 +0300

    dev1

---
+++ 05-8/function.py
@@ -0,0 +1,71 @@
+# print('hii  my name hodi the 3d ')
+# x = input('gooog')
+#
+# len([2,5,333,24])
+import random
+
+
+def royee():
+    print('yawzaaa')
+
+
+def greet(my_name):
+    print(f'hii {my_name}')
+
+
+def print_fullname(first_name, lastname):
+    print(f'welcome {first_name}  {lastname}')
+
+
+def print_area(a, b, c, h):
+    print(f' the area is {h * c / 2}')
+
+
+def sum(x, y):
+    return x + y
+
+
+def max1(x, y):
+    # return x if x > y else y
+    if x > y:
+        return x
+    return y
+
+
+def get_random():
+    r = random.randint(0, 100)
+    big_r = max1(r, 20)
+    return big_r
+
+
+s1 = sum(4, 6)
+list_len = len([22, 22, 2, 2])
+
+print(max1(10, 6))
+
+print(get_random())
+
+#
+# print_area(c=5, a=4, b=22, h=1)
+# print_area(5, 4, 22, 1)
+#
+# royee()
+# print_fullname('hodi', 'zoubi')
+# name = 'sveta'
+# greet(name)
+# greet('jack')
+
+# name = input('enter your name :')
+# print(f'hi {name}')
+#
+# print('3+3=?')
+# x=input()
+#
+# name = input('enter your name :')
+# print(f'hi {name}')
+#
+# print(len(name))
+#
+# name = input('enter your name :')
+# print(f'hi {name}')
+# print('see you next week')
