f38fa3640f07e21a184db0f00485db47abb07e0d
“hodi <hothaifazoubi@gmail.com>
2022-08-05 12:55:34 +0300

    func

--- 05-8/homework_sol.py
+++ 05-8/homework_sol.py
@@ -36,23 +36,23 @@
 # print(board[2][1])
 #
 
-board2 = [['x', 'x', 'x'],
-          ['o', 'x', '-'],
-          ['o', 'x', 'o']]
-
-
-
-y=1
-if (board2[y][y] == board2[y-1][y-1] == board2[y+1][y+1] and board2[y][y] != '-') \
-        or (board2[y][y] == board2[y-1][y +1] == board2[y + 1][y - 1] and board2[y][y] != '-'):
-    print('win')
-else:
-    for x in range(3):
-        if board2[x][0] == board2[x][1] == board2[x][2] and board2[x][0] != '-':
-            print('row-----win')
-
-        elif board2[0][x] == board2[1][x] == board2[2][x] and board2[0][x] != '-':
-            print('column ---- win')
+# board2 = [['x', 'x', 'o'],
+#           ['o', 'o', '-'],
+#           ['o', 'o', 'x']]
+#
+#
+#
+# y=1
+# if (board2[y][y] == board2[y-1][y-1] == board2[y+1][y+1] and board2[y][y] != '-') \
+#         or (board2[y][y] == board2[y-1][y +1] == board2[y + 1][y - 1] and board2[y][y] != '-'):
+#     print('win')
+# else:
+#     for x in range(3):
+#         if board2[x][0] == board2[x][1] == board2[x][2] and board2[x][0] != '-':
+#             print('row-----win')
+#
+#         elif board2[0][x] == board2[1][x] == board2[2][x] and board2[0][x] != '-':
+#             print('column ---- win')
 
 # elif board2[y][y] == board2[y-1][y +1] == board2[y + 1][y - 1] and board2[y][y] != '-':
 #     print('win')
@@ -63,3 +63,64 @@ else:
 #     print('winner')
 # if board2[2][0] == board2[2][1] == board2[2][2]and board2[2][0] != '-':
 #     print('winner')
+
+# while true:
+#     statement1
+#     statement2
+#     statement3
+#     if cond : break;
+
+
+# do-while
+# while True:
+#     print('press 1 to deposit')
+#     print('press 2 to withdraw')
+#     print('press q to to exit ')
+#     user_input = input()
+#     if user_input == '1':
+#         print('pending 1....')
+#     elif user_input == '2':
+#         print('pending 2....')
+#     elif user_input == 'q':
+#         break
+#
+# #while
+# x = 5
+# multi = 1
+#
+# while x != 0:
+#     multi *= x
+#     x -= 1
+# print(multi)
+
+
+# counter = 0
+# for grade in [55, 77, 100, 23]:
+#     if grade < 60:
+#         continue
+#         #break  - counter will be 0
+#     counter += 1
+#
+# print(counter)
+
+
+# 0 7 14 21 28 35 42 ......100
+
+# for i in range(100):
+#     if i % 7 == 0:
+#         print(i)
+
+# for i in range(7,100,7):
+#     print(i)
+
+# print( list(range(7,101,7)))
+
+
+# sum=0
+# while True:
+#     numb1 = int(input('enter a number'))
+#     if numb1 < 0:
+#         break
+#     sum+=numb1
+# print(sum)
+
