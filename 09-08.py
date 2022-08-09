e9678ae9b250ef51355abff73c109d4d5c4b5663
“hodi <hothaifazoubi@gmail.com>
2022-08-09 18:27:19 +0300

    functions1

---
+++ 09-08.py
@@ -0,0 +1,51 @@
+# def avg():
+#     print(95)
+#
+# def get_avg():
+#     return 'hii'
+#
+# avg()
+#
+# x = print('2')
+#
+# a=get_avg()
+# b=get_avg()
+#
+# print(a+b)
+
+# def greet(firstname = None , lastname = '' ):
+#     print(f'hello sir , good morning {firstname} {lastname}')
+#
+#
+# greet('ron' , 'yanai' )
+# greet('kobi','nana')
+#
+# greet('yuval')
+# greet(lastname='zoubi')
+# greet(firstname='amit',lastname='yawza')
+# greet(lastname='yawza',firstname='amit')
+# greet()
+
+
+# def power(x ,y ):
+#     return x**y
+#
+# ans = power(2,2)
+#
+# print(ans)
+#
+# r = round(5.2)
+# print(r)
+#
+# name_len=len('alon')
+# print(name_len)
+
+
+# def hello_with_name(name):
+#     print(name)
+#
+# hello_with_name('ella')
+#
+
+# write a python function
+
