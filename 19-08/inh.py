a5e31f38a04bdd402bddcdb024dc7b91d52b9b6f
“hodi <hothaifazoubi@gmail.com>
2022-08-19 11:28:07 +0300

    inh

---
+++ 19-08/inh.py
@@ -0,0 +1,30 @@
+# Don't repeat yourself
+
+# class Animal:  # super class
+#     def eat(self):
+#         print('eating')
+#     def make_Sound(self):
+#         print('ooooooo')
+#     def __str__(self):
+#         return f'Animal class str'
+#
+#
+# class Dog(Animal):  # subclass
+#     def make_Sound(self):
+#         print('wroooph wrooooph')
+#     def __str__(self):
+#         return 'Dog class'
+#
+#
+# class Cat(Animal):
+#     def jump(self):
+#         print('jumping ...')
+#
+#
+# d1 = Dog()
+# d1.eat()
+# d1.make_Sound()
+# print(d1)
+#
+# c1 = Cat()
+# c1.eat()
